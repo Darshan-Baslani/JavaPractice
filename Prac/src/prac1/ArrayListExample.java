ee1caf0edda060d94a9943ee0794b84cbb2c98e4
Darshan-Baslani <dcbaslani@gmail.com>
2023-05-01 16:35:11 +0530

    Array List

---
+++ Prac/src/prac1/ArrayListExample.java
@@ -0,0 +1,19 @@
+package prac1;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ArrayListExample {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        ArrayList<Integer> aList = new ArrayList<>();
+
+        for(int i=0; i<5; i++) {
+            aList.add(in.nextInt());
+        }
+        for(int i=0; i<5; i++) {
+            System.out.println(aList.get(i));
+        }
+
+    }
+}
