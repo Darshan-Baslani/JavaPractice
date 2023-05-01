d3d3784acd3f21ff12c93cfc38cd354956f873f0
Darshan-Baslani <dcbaslani@gmail.com>
2023-05-01 16:07:49 +0530

    Array Practice

---
+++ Prac/src/prac1/array2D.java
@@ -0,0 +1,21 @@
+package prac1;
+
+import java.util.Arrays;
+
+public class array2D {
+    public static void main(String[] args) {
+        int[][] arr = {
+                {1, 2, 3},
+                {4, 5},
+                {6, 7, 8, 9}
+        };
+
+        for (int[] ints : arr) {
+            for (int anInt : ints) {
+                System.out.print(anInt + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
