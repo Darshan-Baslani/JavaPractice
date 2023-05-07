ac08f6356bb46fba669249ea542674ef410eddac
Darshan-Baslani <dcbaslani@gmail.com>
2023-05-07 20:15:05 +0530

    Infinte Array in Binary Search

---
+++ Prac/src/prac1/InfiniteArrayBS.java
@@ -0,0 +1,32 @@
+package prac1;
+//This program is designed for infinite elements in the array, may not work properly in finite elements.
+public class InfiniteArrayBS {
+    public static void main(String[] args) {
+        int[] arr = new int[]{3, 5, 7, 9, 10, 90,
+                100, 130, 140, 160, 170};
+        int target = 10;
+        int start = 0, end = 1;
+        int ans=-1;
+        while(target > arr[end]) {
+            int temp = end + 1;
+            end = end + (end - start + 1) * 2;
+            start = temp;
+        }
+        ans = Binary_Search(arr, target, start, end);
+        System.out.println(ans);
+    }
+    static int Binary_Search(int[] arr, int target, int start, int end) {
+
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
+            if(arr[mid] > target) {
+                end = mid-1;
+            } else if (arr[mid] < target) {
+                start = mid+1;
+            }else{
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
