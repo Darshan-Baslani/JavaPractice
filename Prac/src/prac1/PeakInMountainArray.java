8b3d87c9ec46c7ebcf73f47f4d893374c5bc6a73
Darshan <dcbaslani@gmail.com>
2023-05-21 14:31:01 +0530

    Peak in Mountain Array

---
+++ Prac/src/prac1/PeakInMountainArray.java
@@ -0,0 +1,22 @@
+package prac1;
+//leetcode - 852 - medium
+//https://leetcode.com/problems/peak-index-in-a-mountain-array/
+public class PeakInMountainArray {
+    public static void main(String[] args) {
+        int[] arr = {0,10,5,2};
+        System.out.println(peakIndexInMountainArray(arr));
+    }
+    public static int peakIndexInMountainArray(int[] arr) {
+        int start = 0, end = arr.length - 1, maxIndex = 0;
+        while(start < end) {
+            int mid = start + (end - start) / 2;
+            if(arr[mid] > arr[mid + 1]) {
+                end = mid;
+            }else{
+                start = mid + 1;
+            }
+        }
+
+        return start;
+    }
+}
